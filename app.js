b786e7e8d45ffaa3e5e1b0d6b856dbbd65a90e94
joel-noronha <joel.noronha0912@gmail.com>
2026-01-27 21:19:31 +0530

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDoc = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -32,8 +37,11 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Jobs API</h1>");
+  res.send(
+    "<h1>Jobs API</h1><a href='/api-docs'>Swagger API Documentation</a>",
+  );
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 // routes
 app.use("/api/v1/auth", authRouter);
